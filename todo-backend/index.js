1b478f1d5a153cae361a48d25796b847e7a7503e
AYceren11 <drayceren@gmail.com>
2026-04-04 09:09:57 +0300

    Backend ve Frontend tamamlandı

---
+++ todo-backend/index.js
@@ -0,0 +1,45 @@
+import express from "express";
+import cors from "cors";
+import { pool } from "./db.js";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Tüm görevleri getir
+app.get("/tasks", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM tasks ORDER BY id ASC");
+    res.json(result.rows);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// Yeni görev ekle
+app.post("/tasks", async (req, res) => {
+  try {
+    const { title } = req.body;
+    const result = await pool.query(
+      "INSERT INTO tasks (title) VALUES ($1) RETURNING *",
+      [title]
+    );
+    res.json(result.rows[0]);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// Görev Silme (DELETE)
+app.delete("/tasks/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    await pool.query("DELETE FROM tasks WHERE id = $1", [id]);
+    res.json({ message: "Görev silindi!" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Backend ${PORT} portunda fıstık gibi çalışıyor!`));
